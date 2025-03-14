0e6e21348d7871e0c98d819ad1ce2ac2fbd3a4d8
unknown <ajaykumar23aps@gmail.com>
2025-03-14 10:47:51 +0530

    Test Case 5: Handle negative numbers

--- calculator.js
+++ calculator.js
@@ -9,7 +9,12 @@ function add(numbers) {
     }
 
     const numArray = numbers.split(delimiter).map(Number);
+
+    const negatives = numArray.filter(num => num < 0);
+    if (negatives.length > 0) {
+        throw new Error(`Negatives not allowed: ${negatives.join(", ")}`);
+    }
+
     return numArray.reduce((sum, num) => sum + num, 0);
 }
 module.exports = { add };
-
--- calculator.test.js
+++ calculator.test.js
@@ -1,6 +1,6 @@
 const { add } = require('./calculator');
 
-test('supports custom delimiters', () => {
-    expect(add("//;\n1;2")).toBe(3);
+test('throws an error when negative numbers are used', () => {
+    expect(() => add("1,-2,3")).toThrow("Negatives not allowed: -2");
 });
 
